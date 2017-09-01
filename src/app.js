4f64b1b5e9f7cbc5afe89e16fa75b41261a076e5
Gustavo Azambuja <gustavoa@overactiveinc.com>
2017-09-01 11:08:27 -0300

    first version

--- src/app.js
+++ src/app.js
@@ -382,8 +382,7 @@ class FacebookBot {
 
         return new Promise((resolve, reject) => {
             request({
-                url: 'https://graph.facebook.com/v2.6/me/messages',
-                qs: {access_token: FB_PAGE_ACCESS_TOKEN},
+                uri: `https://graph.facebook.com/v2.6/me/messages?access_token=${FB_PAGE_ACCESS_TOKEN}`,
                 method: 'POST',
                 json: {
                     recipient: {id: sender},
